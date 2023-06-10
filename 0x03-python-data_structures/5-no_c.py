20abf95c6f17a43a51e8cb8cb32e15c64a33e943
jilan.elsherbiny <jilan.elsherbiny@gmail.com>
2023-06-10 15:51:27 -0700

    task 6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        newlist = list(my_string)
-        [newlist.remove(alph) for alph in newlist if alph == 'c' or alph == 'C']
-        return("".join(newlist))
+    new_string = ""
+    for c in my_string:
+        if (c != 'c') and (c != 'C'):
+            new_string += c
+    return new_string
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        rows = []
-        for row in matrix:
-            new_str = " ".join(str(i) for i in row)
-            ele = "".join(new_str)
-            print("{:s}".format(ele))
+    for row in matrix:
+        for i in row:
+            print("{:d}".format(i), end=" " if i != row[-1] else "")
+        print()
