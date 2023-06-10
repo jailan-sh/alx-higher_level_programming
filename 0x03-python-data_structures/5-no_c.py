eeacca28c5d7222afcf408b0475e64cadad4cdb3
jilan.elsherbiny <jilan.elsherbiny@gmail.com>
2023-06-10 16:10:53 -0700

    12

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for c in my_string:
-        if (c != 'c') and (c != 'C'):
+        if (c != 'c') or (c != 'C'):
             new_string += c
-    return new_string
+    return (new_string)
