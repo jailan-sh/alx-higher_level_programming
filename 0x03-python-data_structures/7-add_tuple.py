5f314bb3dd5741ef2bdeec38b08c8187a26cd3a6
jilan.elsherbiny <jilan.elsherbiny@gmail.com>
2023-06-11 11:31:52 -0700

    task 7another answer

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     newtuple = ()
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        newtuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif len(tuple_a) >= 2 and len(tuple_b) == 1:
-        newtuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
-    elif len(tuple_a) >= 2 and len(tuple_b) == 0:
-        newtuple = (tuple_a[0], tuple_a[1])
-    elif len(tuple_a) == 1 and len(tuple_b) >= 2:
-        newtuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
-    elif len(tuple_a) == 0 and len(tuple_b) >= 2:
-        newtuple = (tuple_b[0], tuple_b[1])
-    elif len(tuple_a) == 0 and len(tuple_b) == 0:
-        newtuple = (0, 0)
-    elif len(tuple_a) == 1 and len(tuple_b) == 0:
-        newtuple = (tuple_a[0], 0)
-    elif len(tuple_a) == 0 and len(tuple_b) == 1:
-        newtuple = (tuple_b[0], 0)
+
+    if len(tuple_a) == 0:
+        tuple_a = (0, 0)
+    elif len(tuple_a) == 1:
+        tuple_a = (tuple_a[0], 0)
+
+    if len(tuple_b) == 0:
+        tuple_b = (0, 0)
+    elif len(tuple_b) == 1:
+        tuple_b = (tuple_b[0], 0)
+
+    newtuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return newtuple
