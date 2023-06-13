9e6e543cf70ac4be2fbab30dd4c9f661cc00050e
jilan.elsherbiny <jilan.elsherbiny@gmail.com>
2023-06-12 17:51:41 -0700

    replace

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if n > len(str) - 1 or n < ((len(str) -1) * -1):
+    if n > len(str) - 1 or n < 0:
         return (str)
     else:
         newstr = str.replace(str[n], "")
