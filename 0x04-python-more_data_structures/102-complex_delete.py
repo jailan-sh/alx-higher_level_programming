d4f33694907e90878e77daf81f36ad4d3e35aec7
jilan.elsherbiny <jilan.elsherbiny@gmail.com>
2023-06-16 08:36:25 -0700

    102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys = []
+    for key in a_dictionary:
+        if a_dictionary[key] == value:
+            keys.append(key)
+    for key in keys:
+        a_dictionary.pop(key)
+    return (a_dictionary)
