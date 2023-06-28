578fced8bc2af94db14da6e0d6790a5ae8ef304e
jailan-sh <jilan.elsherbiny@gmail.com>
2023-06-27 17:07:22 -0700

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" defined class square"""
+
+
+class Square:
+    """ class square."""
+    def __init__(self, size=0):
+        """Private instance attribute: size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """ returns the current square area. """
+        return self.__size ** 2
