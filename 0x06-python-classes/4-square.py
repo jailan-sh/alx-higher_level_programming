b07c77ec7f44fa89520ede53f0622bb79a8500c6
jailan-sh <jilan.elsherbiny@gmail.com>
2023-06-27 17:44:35 -0700

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,6 +13,21 @@ class Square:
         else:
             self.__size = size
 
+    @property
+    def size(self):
+        """ getter """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
     def area(self):
         """ returns the current square area. """
         return self.__size ** 2
