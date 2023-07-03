3d549cdd88fba84d5be87d64e4ce804c35eba049
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-03 11:35:56 -0700

    2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" define rectangle class """
+
+
+class Rectangle:
+    """ a class rectangle """
+    def __init__(self, width=0, height=0):
+        """ constructor of rectangle class
+        Private instance attribute: width height """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """ to retrieve width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ method to set width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ method to set height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ method to get area """
+        return self.height * self.width
+
+    def perimeter(self):
+        """ method to get primeter """
+        if self.height == 0 or self.width == 0:
+            return 0
+        else:
+            return ((self.width + self.height) * 2)
