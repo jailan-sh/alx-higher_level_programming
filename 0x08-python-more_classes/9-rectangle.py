5b972cbc87f7ed5894ef6ca580096ee84de408d5
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-04 03:42:36 -0700

    height

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
     def width(self, value):
         """ method to set width """
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
@@ -37,9 +37,9 @@ class Rectangle:
     def height(self, value):
         """ method to set height"""
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if (value < 0):
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__height = value
 
     def area(self):
@@ -90,4 +90,5 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """ class method constructor """
-        return cls(size, size)
+        rec = cls(height = size, width = size)
+        return rec
