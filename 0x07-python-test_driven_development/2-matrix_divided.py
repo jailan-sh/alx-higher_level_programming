5f6d9a5272b8003dc68737abd4172fa1b107e588
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-04 16:06:45 -0700

    2

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+
+Divide a matrix
+
+"""
+
+
+def matrix_divided(matrix, div):
+    """function that divides all elements of a matrix.
+    matrix must be a list of lists of integers or floats
+    otherwise raise a TypeError"""
+
+    
+    return new
