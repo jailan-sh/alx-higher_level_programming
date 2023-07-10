fdb68d26612f3d453e950b8d6c680db4845b1fe1
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-10 06:47:04 -0700

    5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,8 @@
 
 def inherits_from(obj, a_class):
     """
-     function that returns True if the object is an instance of a class that inherited
+     function that returns True
+     if the object is an instance of a class that inherited
      (directly or indirectly) from the specified class ; otherwise False.
     """
     return False if type(obj) == a_class else isinstance(obj, a_class)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class BaseGeometry."""
+
+
+class BaseGeometry:
+    """
+    an empty class BaseGeometry
+    """
+    pass
