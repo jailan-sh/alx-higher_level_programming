ca4c99278320d36825cab918827e13f4dab98042
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-10 06:13:06 -0700

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" class MyList that inherits from list"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    a function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from
+    """
+
+    return isinstance(obj, a_class)
