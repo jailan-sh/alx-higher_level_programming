86ca6d48f5500d5c46a88c4dfebfa34e198ce5e3
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-11 05:15:59 -0700

    fix files

--- 0x0A-python-inheritance/10-main.py
+++ 0x0A-python-inheritance/10-main.py
@@ -5,3 +5,6 @@ s = Square(13)
 
 print(s)
 print(s.area())
+Square = __import__('10-square').Square
+Rectangle = __import__('9-rectangle').Rectangle
+print(issubclass(Square, Rectangle))
--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,25 +5,7 @@
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
-class Rectangle(BaseGeometry):
-    """
-    inhertance class Rectangle
-    """
-    def __init__(self, width, height):
-        """ constractor for inhertance class Rectangle """
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """ Public instance method """
-
-        return self.__height * self.__width
-
-    def __str__(self):
-        """ Method for when print is used """
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
