0ccb522b71d7bb1aa1a694ca5b30d9d8a01cf479
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-11 15:12:24 -0700

    1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
+""" read file"""
 
 
 def read_file(filename=""):
-    """ function that reads a text file (UTF8) and prints it to stdout """
-
-    with open(filename, encoding="utf-8") as myfile:
+    """
+    function that reads a text file (UTF8) and prints it to stdout
+    args : filename
+    """
+    with open(filename, "r", encoding="utf-8") as myfile:
         print(myfile.read(), end='')
