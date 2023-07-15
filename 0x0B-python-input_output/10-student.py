d4dde2e389b809e525b3ed3810ce3f71cde3a9b9
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-15 02:17:57 -0700

    10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" class module """
+
+
+class Student:
+    """ class student """
+
+    def __init__(self, first_name, last_name, age):
+        """ constructor """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance"""
+        if not isinstance(attrs, list) or \
+           not all(isinstance(i, str) for i in attrs):
+            mydic = self.__dict__.copy()
+        else:
+            mydic = {i: self.__dict__[i] for i in attrs if i in self.__dict__}
+        return mydic
