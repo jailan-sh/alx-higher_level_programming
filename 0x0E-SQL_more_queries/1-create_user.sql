90369aff2b14e86ceeaf795ea62a5b2e868d9c88
jailan-sh <jilan.elsherbiny@gmail.com>
2023-08-17 16:18:30 -0700

    task

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- creates the MySQL server user user_0d_1
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
-      IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+      IDENTIFIED BY 'user_0d_1_pwd';
