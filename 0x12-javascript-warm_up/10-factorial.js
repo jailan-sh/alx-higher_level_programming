6572012b8423ee94ff68373d87a4aa268912d93e
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-12 01:16:33 +0300

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,3 +1,15 @@
 #!/usr/bin/node
 
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+}
 
+console.log(factorial(Number(process.argv[2])));
