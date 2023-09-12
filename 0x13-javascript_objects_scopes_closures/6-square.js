5a040a00fb5068b6fa8c5918fa1e5e21ad852043
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-13 00:31:37 +0300

    square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+module.exports = class Square extends Square1 {
+  charPrint (c) {
+    let myChar;
+    if (c === undefined) {
+      myChar = 'X';
+      for (let i = 0; i < this.width - 1; i++) {
+        myChar += 'X';
+      }
+    } else {
+      myChar = c;
+      for (let i = 0; i < this.width - 1; i++) {
+        myChar += c;
+      }
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(myChar);
+    }
+  }
+};
