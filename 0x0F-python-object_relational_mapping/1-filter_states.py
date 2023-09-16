07e376d9f0d736885f910e4526b4909b98922a67
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-16 20:52:38 +0300

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa"""
+"""Lists all states from the database hbtn_0e_0_usa"""
 
 
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    mydb = MySQLdb.connect(host="localhost",
-                       user=sys.argv[1],
-                       passwd=sys.argv[2],
-                       port=3306,
-                       db=sys.argv[3]
-                       )
+    mydb = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        port=3306,
+        db=sys.argv[3]
+    )
     mycursor = mydb.cursor()
-    
-    mycursor.execute("SELECT states.id, name FROM states WHERE name "
-                "COLLATE latin1_general_cs "
-                "LIKE 'N%' "
-                "ORDER BY states.id ASC;")
-    rows = mycuror.fetchall()
+    sql = "SELECT states.id, name FROM states ORDER BY states.id ASC"
+    mycursor.execute(sql)
+    rows = mycursor.fetchall()
     for row in rows:
-        print(row)
-mycursor.close()
-mydb.close()
+        if row[1][0] == 'N':
+            print(row)
+    mycursor.close()
+    mydb.close()
