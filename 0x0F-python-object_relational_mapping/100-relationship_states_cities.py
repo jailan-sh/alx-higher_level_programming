4e3fdfeb4f6d8738f08f8f557e24a0ab34dd4eab
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-18 02:55:56 +0300

    100

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from model_city import Base, City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    session = sessionmaker(bind=engine)
+    session = session()
+    records = session.query(State, City)\
+        .join(City, City.state_id == State.id)\
+        .order_by(City.id).all()
+    for state, city in records:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
