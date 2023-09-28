de997c0543318458f5cf666b212ec9e2b514a265
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-28 15:27:53 -0700

    s

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
-#!usr/bin/env bash
+#!/bin/bash
 #sends a request to URL, and displays the size of the body of the response
-
-url=$1
-
-curl -Is -w "%{content_lenght}\n" "$1url"
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
