b5006dd4b2ec24f951b1b77ee03acde80533d242
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-29 14:57:30 -0700

    task7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     import sys
     url = sys.argv[1]
     r = requests.get(url)
-    print(r)
-    print("Error code: {}".format())
+    if r.status_code == requests.codes.ok:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
