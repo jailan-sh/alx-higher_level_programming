d39c1427ca9b6ae46e3a267550990daa2e1200bb
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-29 04:01:01 +0300

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,11 @@
 
 def find_peak(list_of_integers):
     """finds a peak"""
-
+    if len(list_of_intege) == 0:
+        return None
+    elif len(list_of_intege) == 1:
+        return list_of_intege[0]
+    else:
+        return (max(list_of_intege))
     
 
