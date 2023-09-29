9f11fc0a5812fb6f82df527a6e3fcd1b4ac0bbc5
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-30 01:40:47 +0300

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,10 +4,16 @@
 if __name__ == "__main__":
     import requests
     import sys
-    if len(sys.args) == 1:
+    if len(sys.argv) == 1:
         pyload = {'q': ""}
     else:
         payload = {'q': sys.argv[1]}
     r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
-    data_dic = r.json()
-    print("[{}] {}".format(data_dic[id], data_dic[name])
+    try:
+        r_dict = r.json()
+        if r_dict = {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict[id], r_dict[name])
+    except ValueError:
+        print("Not a valid JSON")
