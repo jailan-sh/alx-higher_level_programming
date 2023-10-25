063a33bef6c1faa838379720a63d4baeabe5ca67
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-26 02:14:41 +0300

    2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2], (err, res, body) => {
-  console.log('code:', res.statusCode);
-});
+request
+  .get(process.argv[2])
+  .on('response', function (response) {
+    console.log('code:', response.statusCode);
+  });
