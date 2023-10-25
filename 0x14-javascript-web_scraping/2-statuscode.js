088d52bd35cc5301140e1bb7424d1c5564870303
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-26 02:40:01 +0300

    push

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-
-const url = process.argv[2];
-request.get(url, (err, res, body) => {
-  if (err) {
-
-  } else {
-    console.log('code: ', res.statusCode);
-  }
+request.get(process.argv[2], (response) => {
+  console.log('code:', response.statusCode);
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-request
-  .get(process.argv[2])
-  .on('response', function (response) {
-    console.log('code:', response.statusCode);
-  });
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
