afd02607c28ad71b9e64985446dd04b6da112b3b
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-26 02:42:39 +0300

    2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2], (response) => {
-  console.log('code:', response.statusCode);
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
